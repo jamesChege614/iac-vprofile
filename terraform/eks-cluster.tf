42c0a1be07e8bf1e31dddc6aee4bac675cea9cfc
jamesChege614 <mbuguajames538@gmail.com>
2025-08-23 20:05:24 +0300

    just testing out.

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -6,12 +6,15 @@ module "eks" {
   cluster_version = "1.27"
 
   vpc_id                         = module.vpc.vpc_id
+
   subnet_ids                     = module.vpc.private_subnets
+
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
+    
     ami_type = "AL2_x86_64"
-
+    
   }
 
   eks_managed_node_groups = {
